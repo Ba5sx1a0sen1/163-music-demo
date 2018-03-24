f34c53a3a090bf0e7bdb67f6f30e0b9dbcbc1c44
BassXiaoSen <314121378@qq.com>
2018-03-25 03:23:09 +0800

    添加更新编辑功能

--- src/js/song-form.js
+++ src/js/song-form.js
@@ -29,19 +29,19 @@
         </form>
         `,
         render(data = {}) {
-            let placeholders = ['name', 'singer','url']
+            let placeholders = ['name', 'singer', 'url']
             let html = this.template
             placeholders.map((string) => {
                 html = html.replace(`__${string}__`, data[string] || ``)
             })
             $(this.el).html(html)
-            if(data.id){
+            if (data.id) {
                 $(this.el).prepend('<h1>编辑歌曲</h1>')
-            }else{
+            } else {
                 $(this.el).prepend('<h1>新建歌曲</h1>')
             }
         },
-        reset(){
+        reset() {
             this.render({})
         }
     }
@@ -52,15 +52,25 @@
             var song = new Song();
             song.set('name', data.name);
             song.set('singer', data.singer);
-            song.set('url', data.url);   
-            return song.save().then((newSong)=> {
-                let {id,attributes} = newSong
+            song.set('url', data.url);
+            return song.save().then((newSong) => {
+                let { id, attributes } = newSong
                 // Object.assign(this.data,{id,...attributes}) 使用到了旧的内存，要结合深拷贝来做
-                this.data = {id,...attributes}
+                this.data = { id, ...attributes }
                 console.log(newSong);
-            },  (error) => {
+            }, (error) => {
                 console.error(error);
             });
+        },
+        update(data){
+            var song = AV.Object.createWithoutData('Song', this.data.id);
+            song.set('name', data.name);
+            song.set('singer',data.singer)
+            song.set('url',data.url)
+            return song.save().then((Response)=>{
+                Object.assign(this.data,data)
+                return Response
+            })
         }
     }
     let controller = {
@@ -69,17 +79,17 @@
             this.model = model
             this.view.render(this.model.data) //this是this.view 调用得到前面定义的函数
             this.bindEvents()
-            window.eventHub.on('select',(data)=>{
+            window.eventHub.on('select', (data) => {
                 this.model.data = data
                 this.view.render(this.model.data)
             })
-            window.eventHub.on('new',(data)=>{
-                if(this.model.data.id){
+            window.eventHub.on('new', (data) => {
+                if (this.model.data.id) {
                     this.model.data = {
-                        name:'',url:'',id:'',singer:''
+                        name: '', url: '', id: '', singer: ''
                     }
-                }else{
-                    Object.assign(this.model.data,data) //留下自定义bug
+                } else {
+                    Object.assign(this.model.data, data) //留下自定义bug
                 }
                 this.view.render(this.model.data)
             })
@@ -87,21 +97,38 @@
         reset(data) {
             this.view.render(data)
         },
+        create() {
+            let need = ['name', 'singer', 'url']
+            let data = {}
+            need.map((string) => {
+                data[string] = $(this.view.el).find(`[name=${string}]`).val()
+            })
+            this.model.create(data)
+                .then(() => {
+                    this.view.reset()
+                    window.eventHub.emit('create', this.model.data)
+                })
+        },
+        update(){
+            let need = ['name', 'singer', 'url']
+            let data = {}
+            need.map((string) => {
+                data[string] = $(this.view.el).find(`[name=${string}]`).val()
+            })
+            this.model.update(data)
+                .then(()=>{
+                    alert('更新成功')
+                    window.eventHub.emit('update',JSON.parse(JSON.stringify(this.model.data)))
+                })
+        },
         bindEvents() {
             $(this.view.el).on('submit', 'form', (e) => {
                 e.preventDefault()
-
-
-                let need = ['name', 'singer', 'url']
-                let data = {}
-                need.map((string) => {
-                    data[string] = $(this.view.el).find(`[name=${string}]`).val()
-                })
-                this.model.create(data)
-                    .then(()=>{
-                        this.view.reset()
-                        window.eventHub.emit('create',this.model.data)
-                    })
+                if (this.model.data.id) {
+                    this.update()
+                } else {
+                    this.create()
+                }
             })
         }
     }
